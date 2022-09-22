99d4d9a0163b987ddfa804da14c0c16f5dca9deb
safouanaou <aouezgharsafouan@gmail.com>
2022-09-22 20:18:01 +0100

    added a GameBoardModule module function

---
+++ script.js
@@ -0,0 +1,22 @@
+const container = document.querySelector('.container');
+const playerOneText = document.getElementById('player1');
+const playerTwoText = document.getElementById('player2');
+const buttonSymbol = document.querySelectorAll('.symbol');
+const replay = document.getElementById('replay');
+const winner = document.getElementById('winner');
+const displayWinnerPage = document.querySelector('.reload-winner')
+const turnText = document.querySelector('.turnText');
+const box = Array.from(document.querySelectorAll('.box'));
+
+
+(function gameBoardModule(){
+
+
+})()
+
+
+
+
+
+
+
