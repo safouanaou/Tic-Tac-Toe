d9f90ce96b46b53f333935582db6980fbf1e857e
safouanaou <aouezgharsafouan@gmail.com>
2022-09-22 20:24:01 +0100

    added a checkWinner function that takes three parameter numbers for each box in a row or column and checks if their innerHTML is the same. if so it returns the winner. if all elements's innerHTML are not empty yet no row or column passes the test, it returns 'it's a tie'

--- script.js
+++ script.js
@@ -10,7 +10,32 @@ const box = Array.from(document.querySelectorAll('.box'));
 
 
 (function gameBoardModule(){
+    let turn = 'x';
 
+    const gameBoard = box;
+    
+    
+    function checkWinner(num1,num2, num3){
+        
+        let a = gameBoard[num1].innerHTML;
+        let b = gameBoard[num2].innerHTML;
+        let c = gameBoard[num3].innerHTML;
+    
+        if(a === b && b === c && c !== ''){
+            turnText.innerHTML = a.toUpperCase() + ' is the winner';
+            turn = '';
+            winner.innerHTML = a + ' is the winner';
+            displayWinnerPage.style.display = 'flex';        container.style.display = 'none';
+    
+        }else if(gameBoard.every(isEmpty)){
+            winner.innerHTML = "it's a tie";
+            displayWinnerPage.style.display = 'flex';   
+            container.style.display = 'none';
+        }
+    
+        }
+    
+    
 
 })()
 
