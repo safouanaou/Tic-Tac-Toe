d91437df51a23978cbc8c35416f61fb67d69b245
Safouan Aouezghar <aouezgharsafouan@gmail.com>
2025-12-30 18:38:40 +0100

    Refactor GameController to improve playRound and checkWinner logic

--- script.js
+++ script.js
@@ -32,29 +32,49 @@ const GameController = (()=>{
     const playerTwo = player(player_two, 'O');
     const players = [playerOne, playerTwo]
 
-    const playRound = (index) => {
-        while (GameBoard.getBoard().some(column => column === "")){
-            currentPlayerIdex = 0
-            const switchTurn = () => currentPlayerIdex === 0 ? 1 : 0;
-            console.log(`the game has started\n it's ${players[currentPlayerIdex].name}'s turn: `)
-            //let play = prompt("choose a column: ");
-            GameBoard.placeMarker(index, players[currentPlayerIdex].marker)
-            switchTurn();
-            checkWinner();
+    const checkWinner = () => {
+        const winnerCheckArray = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]];
+        let arrayCheck = [];
+
+        const f = (arr) => {
+            if(arr.every(val => val === arr[0])){
+                return 1;
+            }
+            else return 0;
         }
+
+        winnerCheckArray.forEach(line => {
+            line.forEach(col => {
+                arrayCheck.push(GameBoard.getBoard()[col])
+            })
+
+            if(arrayCheck.every(val => val === arrayCheck[0])){
+                console.log("winner")
+            }
+            
+        })
+
+        
+    };
+
+    let currentPlayerIdex = 0
+
+
+    const playRound = (index) => {
+
+         const switchTurn = () => currentPlayerIdex = currentPlayerIdex === 0 ? 1 : 0;
+        console.log(`the game has started\n it's ${players[currentPlayerIdex].name}'s turn: `)
+        //let play = prompt("choose a column: ");
+        GameBoard.placeMarker(index, players[currentPlayerIdex].marker)
+        console.log(GameBoard.getBoard())
+        checkWinner()
+        switchTurn();
     }
 
-    const checkWinner = (() => {
-        const winnerCheckArray = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[2,4,6],[3,4,5],[6,7,8]]
-        winnerCheckArray.forEach(line => {
-            if (GameBoard.getBoard()[line.every(val => val === line[0])]){
-                if (line[0] === "X") console.log(`the winner is: ${playerOne}`);
-                else if (line[0] === "O") console.log(`the winner is ${playerTwo}`);
-            }
-
-        });
-
-    })();
+     return {
+        playRound
+    }
+    
 })();
 
 
